7b9bb66d1bb3aa1757448e69e5bd38b8690f8a50
Nishant Singh <srnikhil3721@gmail.com>
2022-04-17 19:03:19 +0530

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './modules/auth/auth.module';
+import { RevenueController } from './modules/revenue/revenue.controller';
+import { RevenueService } from './modules/revenue/revenue.service';
+import { RevenueModule } from './modules/revenue/revenue.module';
 
 // laod env
 require('dotenv').config({ path: require('find-config')('.env') });
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DB_URI), AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    MongooseModule.forRoot(process.env.DB_URI),
+    AuthModule,
+    RevenueModule,
+  ],
+  controllers: [RevenueController],
+  providers: [RevenueService],
 })
 export class AppModule {}
